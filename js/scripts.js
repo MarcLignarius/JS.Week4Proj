39aad23a4281c681a1a2b6dab62ca5694581c6de
Marc Davies <marcdaviesriot@gmail.com>
2019-04-12 11:06:19 -0700

    add Business Logic for Pizzas: Pizza function and fullDescription

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
-
-// Business Logic
+// Business Logic for site behavior --------------------------------------------
 
 function onlyOne(checkbox) {
     var checkboxes = document.getElementsByName('size')
@@ -8,8 +7,20 @@ function onlyOne(checkbox) {
     })
 }; //This function makes it so that only one size can be selected
 
-var size = [];
-var toppings = [];
+// Business Logic for orderDetails --------------------------------------------
+
+
+
+// Business Logic for Pizzas -------------------------------------------------
+
+function Pizza(size, toppings) {
+  this.size = size,
+  this.toppings = toppings
+}
+
+Pizza.prototype.fullDescription = function() {
+  return ("Size: " + this.size + " Toppings: " + this.toppings + ".");
+}
 
 // User Interface Logic
 
@@ -17,12 +28,14 @@ $(function() {
   $("form#addPizza").submit(function(event){
     event.preventDefault();
     $("input:checkbox[name=size]:checked").each(function() {
+      var size = [];
       size.push($(this).val());
       console.log("size", size);
-    })
+    }) // Tracks the pizza size the user selected
     $("input:checkbox[name=toppings]:checked").each(function() {
+      var toppings = [];
       toppings.push($(this).val());
       console.log("toppings", toppings)
-    })
+    }) // Tracks the pizza toppings the user selected
   })
 });

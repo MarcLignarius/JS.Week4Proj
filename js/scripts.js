ef2a8982e8229a740571d1a5dcdaa451b0605972
Marc Davies <marcdaviesriot@gmail.com>
2019-04-12 09:13:07 -0700

    add function to only allow for one size

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+
+function onlyOne(checkbox) {
+    var checkboxes = document.getElementsByName('size')
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+}; //This function makes it so that only one size can be selected
+
 $(function() {
+
 
 });
